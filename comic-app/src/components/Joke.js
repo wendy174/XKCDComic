9239138b81c5fbb9625dd30735f68d3e75b7f157
wendy <mizukilee@gmail.com>
2022-12-29 14:09:57 -0800

    joke.js plus added sql comic

--- comic-app/src/components/Joke.js
+++ comic-app/src/components/Joke.js
@@ -4,7 +4,7 @@ function Joke({ joke, favoriteHandler }) {
     console.log(joke);
 
     function onFavorite(e) {
-
+        console.log(e)
         //favoriteHandler();
     }
 
@@ -13,7 +13,9 @@ function Joke({ joke, favoriteHandler }) {
         {/* Wendy to work on Joke.js */}
             <div className="p-5 text-bg-dark rounded-3">
                 <h2>{ joke.title }</h2>
-                <p>{ joke.description }</p>
+                <p>{ joke.alt }</p>
+                <img src={joke.img} alt={joke.title} />
+                <br></br>
                 <button className="btn btn-outline-light" type="button" onClick={ onFavorite }>Favorite</button>
             </div>
             <br></br>
