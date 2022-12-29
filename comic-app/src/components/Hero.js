83286e0a89fb104106f37fcb488d88a990b3a3ae
Ann Ferracane <ann@patchventures.com>
2022-12-29 16:01:43 -0500

    added some logo images

--- comic-app/src/components/Hero.js
+++ comic-app/src/components/Hero.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Hero ({ title, description }) {
+function Hero ({ title, description, image }) {
     return (
         <div className="px-4 py-5 my-5 text-center"> 
-            <img className="d-block mx-auto mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" /> 
+            <img className="d-block mx-auto mb-4" src={ image } alt="" width="72" height="57" /> 
             <h1 className="display-5 fw-bold">{ title }</h1> 
             <div className="col-lg-6 mx-auto"> 
                 <p className="lead mb-4">{ description }</p> 
