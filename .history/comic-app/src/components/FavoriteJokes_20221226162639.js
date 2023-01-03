96b3ea98393918e34e49f87d17c9acd57f6df75b
Ann Ferracane <ann@patchventures.com>
2023-01-03 15:58:15 -0500

    final cleanup

--- .history/comic-app/src/components/FavoriteJokes_20221226162639.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function FavoriteJokes() {
-    return ()
-}
-
-export default FavoriteJokes;
--- .history/comic-app/src/components/FavoriteJokes_20221226162640.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function FavoriteJokes() {
-    return (
-        
-    )
-}
-
-export default FavoriteJokes;
